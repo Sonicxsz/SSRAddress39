adc61de1e79706e3b4f9d29652f76b4c32ee3b3d
arbi <arbih@mail.ru>
2023-04-13 23:20:52 +0300

    some changes

--- src/components/ui/menuItem/MenuItem.tsx
+++ src/components/ui/menuItem/MenuItem.tsx
@@ -7,7 +7,7 @@ function MenuItem({ name, img, src }: MenuItemProps) {
         <div className={styles.menuItemWrapper}>
             <a href={src} target="_blank" rel="noreferrer">
                 <h2 className={styles.menuPageTitle}>{name}</h2>
-                <img src={img} alt={name}  />
+                <Image src={img} alt={name} width={400} height={760}  className={styles.img}/>
             </a>
             <div className={styles.line}></div>
         </div>
